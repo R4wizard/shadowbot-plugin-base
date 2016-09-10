8b4dfeea84a65d15d8b8d31f97588056c0b2a5f9
Peter Corcoran <peter@lemondigits.com>
2016-09-10 21:06:25 +0100

    Added internal support for quickly adding and automatic destruction of commands.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 "use strict";
 
 const events = require('events');
+const Shadow = require('shadowbot-core').Interface;
 
 class PluginBase extends events.EventEmitter {
 
 	constructor() {
 		super();
+
+		this._commands = [];
+	}
+
+	command(name, helptext, handler) {
+		this._commands.push(name);
+		Shadow.registerCommandHandler(name, helptext, handler);
+	}
+
+	destroy() {
+		this._commands.forEach(cmd => Shadow.unregisterCommandHandler(cmd));
 	}
 
 }
